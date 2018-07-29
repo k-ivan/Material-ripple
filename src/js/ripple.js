b57ad83ec70a20fbf383d86ee87a11ab6933e5a0
Ivan Kuzmichov <ivan.kuzmichov@gmail.com>
2018-07-29 18:34:38 +0300

    fixes

--- src/js/ripple.js
+++ src/js/ripple.js
@@ -5,8 +5,6 @@ const Ripple = function () {
 
   let module = {};
   let handlerShow;
-  let handlerHide;
-  let ripple;
   let isInit = false;
 
   function createRipple(target) {
@@ -15,6 +13,15 @@ const Ripple = function () {
       'className': 'ripple',
     });
     target.appendChild(rippleEl);
+
+    // remove exist events
+    target.removeEventListener('mouseleave', hideRipple);
+    target.removeEventListener('mouseup', hideRipple);
+    target.removeEventListener('dragend', hideRipple);
+    // and add again
+    target.addEventListener('mouseleave', hideRipple);
+    target.addEventListener('mouseup', hideRipple);
+    target.addEventListener('dragend', hideRipple);
 
     return rippleEl;
   }
@@ -31,57 +38,67 @@ const Ripple = function () {
       target.style.position = 'relative';
     }
 
-
     const bgColor = target.getAttribute('data-ripple-color') || 'rgba(0,0,0, .3)';
     const isCenter = target.hasAttribute('data-ripple-center');
 
     const { width, height, left, top } = target.getBoundingClientRect();
-
     let posX = evt.clientX - left,
         posY = evt.clientY - top;
-
     const dimmension = Math.max(width, height);
-
-    const el = ripple = createRipple(target);
-
     const halfSize = dimmension / 2;
 
-    el.style.left = isCenter ? '50%' : `${posX - halfSize}px`;
-    el.style.top = isCenter ? '50%' : `${posY - halfSize}px`;
-    el.style.width = `${dimmension}px`;
-    el.style.height = `${dimmension}px`;
+    const ripple = createRipple(target);
+    let styles = `
+      top: ${isCenter ? '50%' : (posY - halfSize) + 'px'};
+      left: ${isCenter ? '50%' : (posX - halfSize) + 'px'};
+      width: ${dimmension}px;
+      height: ${dimmension}px;
+      background-color: ${bgColor};
+    `;
     if (isCenter) {
-      el.style.marginTop = `${-halfSize}px`;
-      el.style.marginLeft = `${-halfSize}px`;
+      styles += `
+        margin-top: ${-halfSize}px;
+        margin-left: ${-halfSize}px;
+      `;
     }
-    el.style.backgroundColor = bgColor;
+    ripple.setAttribute('style', styles);
 
     setTimeout(() => {
-      el.style.cssText += `transform: scale(2.5); opacity: 0.5`;
+      ripple.style.cssText += `transform: scale(2); opacity: 0.5`;
     }, 10);
   }
 
-  function hideRipple(sel, evt) {
+  function hideRipple(evt) {
 
-    if (evt.type === 'mouseout' && !evt.target.closest(sel)) return true;
-    if (evt.type === 'mouseup' && evt.which !== 1) return true;
+    const el = this;
+    if (!el) return;
 
-    if (!ripple) return true;
+    let ripple = null;
+
+    const ripples = el.getElementsByClassName('ripple');
+    if (ripples.length > 0) {
+      ripple = ripples[ripples.length - 1]
+    } else {
+      return false;
+    }
 
     const computedDuration = getComputedStyle(ripple)['transition-duration'];
-    const delay = parseFloat(computedDuration) * 1000;
-
-    const el = ripple;
-    ripple = null;
-    const target = el.parentNode;
-    const ripples = target.getElementsByClassName('ripple');
-
-    el.style.opacity = 0;
+    const delayDuration = parseFloat(computedDuration) * 1000;
 
     setTimeout(() => {
-      target.removeChild(el);
-      if (ripples.length < 1) target.style.position = '';
-    }, delay);
+      ripple.style.opacity = 0;
+
+      setTimeout(() => {
+        try {
+          el.removeChild(ripple);
+        } catch(e) {
+          return false;
+        }
+        if (ripples.length < 1) el.style.position = '';
+
+      }, delayDuration)
+
+    }, 0);
   }
 
   module.init = function (selector) {
@@ -92,23 +109,14 @@ const Ripple = function () {
     }
 
     handlerShow = showRipple.bind(null, selector);
-    handlerHide = hideRipple.bind(null, selector);
     document.body.addEventListener('mousedown', handlerShow);
-    document.body.addEventListener('mouseout', handlerHide);
-    document.body.addEventListener('mouseup', handlerHide);
-    document.body.addEventListener('dragstart', handlerHide);
     isInit = true;
   };
 
   module.destroy = function () {
     if (!isInit) return;
     document.body.removeEventListener('mousedown', handlerShow);
-    document.body.removeEventListener('mouseout', handlerHide);
-    document.body.removeEventListener('mouseup', handlerHide);
-    document.body.removeEventListener('dragstart', handlerHide);
     handlerShow = null;
-    handlerHide = null;
-    ripple = null;
     isInit = false;
   }
 
